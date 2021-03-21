90bd214c6c4463cdd1401cd8c52d512553ae5a58
RayStarkMC <raystarkmc@gmail.com>
2021-03-21 20:02:40 +0900

    StorageNetworkAPIのjavadoc記述

--- src/api/java/raven/ravenstorages/api/network/StorageNetworkAPI.java
+++ src/api/java/raven/ravenstorages/api/network/StorageNetworkAPI.java
@@ -2,6 +2,11 @@ package raven.ravenstorages.api.network;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
+/**
+ * 倉庫管理ネットワークのAPIを表すインターフェース。
+ *
+ * <p>このインターフェースを通じてネットワークに対してクエリを発行する事が出来る。
+ */
 @ParametersAreNonnullByDefault
 public interface StorageNetworkAPI {
 }
