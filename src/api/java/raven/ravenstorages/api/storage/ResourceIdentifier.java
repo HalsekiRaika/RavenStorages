c592e395d69e75e44b7b3636df79ab5818235684
RayStarkMC <raystarkmc@gmail.com>
2021-03-30 23:27:48 +0900

    ResourceIdentifier Javadoc更新

--- src/api/java/raven/ravenstorages/api/storage/ResourceIdentifier.java
+++ src/api/java/raven/ravenstorages/api/storage/ResourceIdentifier.java
@@ -1,11 +1,19 @@
 package raven.ravenstorages.api.storage;
 
+import raven.ravenstorages.api.network.query.request.Requirement;
+
 /**
- * 資源の識別子を表すインターフェースです。
+ * 資源の種類の識別子を表すインターフェースです。
  *
  * <p>資源とは倉庫が取り扱うことが出来る物です。アイテムや液体等倉庫で管理される対象を表します。
+ * 型Tが同じである資源は同種の資源と判断されます。
+ * ただしここで同種であるとはスタック可能であるアイテムのようにゲーム内の同じ資源であると言う事を意味しているわけではありません。
+ * 同種であるとは同じ種類の資源同士でスタック可能か否かを調べたり型Tの実装によっては比較を行うことが出来るという事を意味します。
+ * <p>型Tは{@link Requirement}や{@link ResourceStack}等で利用され、倉庫の検索や輸送に利用されます。
  *
  * @param <T> 資源の型
+ * @see Requirement
+ * @see ResourceStack
  */
 public interface ResourceIdentifier<T> {
 
