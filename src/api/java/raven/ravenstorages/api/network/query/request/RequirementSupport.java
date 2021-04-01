77fd1289a459b354d1b9edf4213627dd63d6c549
RayStarkMC <raystarkmc@gmail.com>
2021-04-01 18:39:38 +0900

    Javadoc記述

--- src/api/java/raven/ravenstorages/api/network/query/request/RequirementSupport.java
+++ src/api/java/raven/ravenstorages/api/network/query/request/RequirementSupport.java
@@ -5,9 +5,15 @@ package raven.ravenstorages.api.network.query.request;
  */
 class RequirementSupport {
     static final Requirement<?> ANY = t -> true;
+    static final Requirement<?> NONE = t -> false;
 
     @SuppressWarnings("unchecked")
     static <T> Requirement<T> any() {
         return (Requirement<T>) ANY;
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T> Requirement<T> none() {
+        return (Requirement<T>) NONE;
     }
 }
