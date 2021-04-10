41c7b246b6e5933e207fbf4470005daa7278c5db
RayStarkMC <raystarkmc@gmail.com>
2021-04-10 21:21:09 +0900

    InterfaceCrystal定義

--- src/main/java/raven/ravenstorages/RavenStorages.java
+++ src/main/java/raven/ravenstorages/RavenStorages.java
@@ -1,15 +1,23 @@
 package raven.ravenstorages;
 
+import net.minecraft.block.Block;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.LanguageProvider;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import raven.ravenstorages.client.ClientProxy;
+import raven.ravenstorages.common.blocks.RavenBlocks;
 import raven.ravenstorages.common.proxy.CommonProxy;
 
-@Mod(Settings.MOD_ID)
+import static raven.ravenstorages.Settings.MOD_ID;
+
+@Mod(MOD_ID)
 public class RavenStorages {
     public static final Logger LOGGER = LogManager.getLogger();
 
@@ -20,5 +28,7 @@ public class RavenStorages {
         this.proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> CommonProxy::new);
         this.proxy.initialization();
         this.proxy.attachLifeCycle(eventBus);
+
+        eventBus.addGenericListener(Block.class, RavenBlocks::register);
     }
 }
---
+++ src/main/java/raven/ravenstorages/common/blocks/InterfaceCrystalBlock.java
@@ -0,0 +1,11 @@
+package raven.ravenstorages.common.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+//TODO ブロックの正式名称の決定
+public final class InterfaceCrystalBlock extends Block {
+    InterfaceCrystalBlock() {
+        super(Properties.create(Material.IRON));
+    }
+}
