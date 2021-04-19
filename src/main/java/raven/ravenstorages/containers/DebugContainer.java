b5c64209068520f95e3c23e2204ce90b61236fef
RayStarkMC <raystarkmc@gmail.com>
2021-04-19 22:40:51 +0900

    SlotPositionLessContainer定義

--- src/main/java/raven/ravenstorages/containers/DebugContainer.java
+++ src/main/java/raven/ravenstorages/containers/DebugContainer.java
@@ -2,7 +2,6 @@ package raven.ravenstorages.containers;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketBuffer;
